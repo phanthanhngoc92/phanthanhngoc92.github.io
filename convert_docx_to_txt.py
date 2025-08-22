d08410f54c89f044935a5bf3456a796bccd7040e
huatoan.duong <huatoan.duong@gmail.com>
2025-08-23 00:31:04 +0700

    add markdown

--- convert_docx_to_txt.py
+++ convert_docx_to_txt.py
@@ -2,5 +2,5 @@ import pypandoc
 
 # Example file:
 docxFilename = 'CV_Megan_0525.docx'
-output = pypandoc.convert_file(docxFilename, 'plain', outputfile="CV_Megan_0525.txt")
+output = pypandoc.convert_file(docxFilename, 'md', outputfile="CV_Megan_0525.md")
 assert output == ""
